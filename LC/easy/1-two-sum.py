8b2f82c1a2e0f599b11b534fd196fc341e9af37b
James Zhang <jameszhanga@gmail.com>
2021-05-28 15:57:26 -0500

    runtime: 5.01%, memory: 17.23%....idk

--- LC/easy/1-two-sum.py
+++ LC/easy/1-two-sum.py
@@ -1,12 +1,10 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        out = [None]*2
-        for x in range(len(nums)-1):
-            temp = len(nums)-1-x
-            for y in range(temp):
-                if nums[x] + nums[x+1+y] == target:
-                    out[0]=x
-                    out[1]=x+1+y
-                    
-        return out
-        
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    out = [None]*2
+    for x in range(len(nums)-1):
+        temp = len(nums)-1-x
+        for y in range(temp):
+            if nums[x] + nums[x+1+y] == target:
+                out[0]=x
+                out[1]=x+1+y
+                
+    return out
