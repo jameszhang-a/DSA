739663dd0f6745975682a5e4e094d7085cd18094
James Zhang <jameszhanga@gmail.com>
2021-07-09 17:55:18 -0500

    runtime: 79.19%, memory: 44.37% [Oneliner, doesn't really count since it uses default function]

--- LC/easy/349-intersection-2-arrays.py
+++ LC/easy/349-intersection-2-arrays.py
@@ -17,32 +17,41 @@
 
 # 1 <= nums1.length, nums2.length <= 1000
 # 0 <= nums1[i], nums2[i] <= 1000
+import numpy as np
+
+x = np.array([(1, 2), (3, 4)], dtype=[('a', '<i4'), ('b', '<i4')])
+x['a']
 
 class Solution:
     def intersection(self, nums1, nums2):
-        cur = set()
-        for num in nums1:
-            if self.bSearch(sorted(nums2), num):
-                cur.add(num)
 
-        return cur
+        ############ BINARY SEARCH BAD ##################
+        #     cur = set()
+        #     for num in nums1:
+        #         if self.bSearch(sorted(nums2), num):
+        #             cur.add(num)
 
-    def bSearch(self, arr, tar):
-        l = 0
-        h = len(arr)-1
+        #     return cur
 
-        while l <= h:
-            m = (l+h)//2
-            if tar < arr[m]:
-                h = m-1
-            elif tar > arr[m]:
-                l = m+1
-            else:
-                return 1
-        return 0
+        # def bSearch(self, arr, tar):
+        #     l = 0
+        #     h = len(arr)-1
+
+        #     while l <= h:
+        #         m = (l+h)//2
+        #         if tar < arr[m]:
+        #             h = m-1
+        #         elif tar > arr[m]:
+        #             l = m+1
+        #         else:
+        #             return 1
+        #     return 0
+
+        ############ SIMPLE SETS ######################
+        return set(nums1)&set(nums2)
 
 
 sol = Solution()
-x = sol.intersection([1, 2, 2, 1],
-                     [2, 2])
+x = sol.intersection([1, 2, 2, 1,6,5],
+                     [2, 2,3,5,6,8])
 print(x)
