1595b249fc86333606b10f4e093640e383182542
James Zhang <jameszhanga@gmail.com>
2021-08-20 14:53:20 -0700

    runtime: 53.59, memory: 25.65

---
+++ problems/LC/easy/937-reorder-data-logs.js
@@ -0,0 +1,103 @@
+/*
+You are given an array of logs. Each log is a space-delimited string of words, 
+where the first word is the identifier.
+
+There are two types of logs:
+
+Letter-logs: All words (except the identifier) consist of lowercase English letters.
+Digit-logs: All words (except the identifier) consist of digits.
+Reorder these logs so that:
+
+The letter-logs come before all digit-logs.
+The letter-logs are sorted lexicographically by their contents. 
+If their contents are the same, then sort them lexicographically by their identifiers.
+The digit-logs maintain their relative ordering.
+Return the final order of the logs.
+
+ 
+
+Example 1:
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+Explanation:
+The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
+The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
+
+
+Example 2:
+Input: logs = ["a1 9 2 3 1",
+                "g1 act car",
+                "zo4 4 7",
+                "ab1 off key dog",
+                "a8 act zoo"]
+                
+Output: ["g1 act car",
+        "a8 act zoo",
+        "ab1 off key dog",
+        "a1 9 2 3 1","
+        zo4 4 7"]
+ 
+
+Constraints:
+
+1 <= logs.length <= 100
+3 <= logs[i].length <= 100
+All the tokens of logs[i] are separated by a single space.
+logs[i] is guaranteed to have an identifier and at least one word after the identifier.
+*/
+
+// Requirments
+// move all letter logs to the front
+// keep digit ordering
+// sort letter logs
+
+// Steps
+// go through each log
+// make copy of log minus the tag
+// separate into two arrays
+// compare letter, if the same, compare the original
+
+/**
+ * @param {string[]} logs
+ * @return {string[]}
+ */
+var reorderLogFiles = function(logs) {
+  digitLog = [];
+  letterLog = [];
+
+  // Separate differnt logs
+  logs.forEach((log) => {
+    // if the first entry after the tag is a letter
+    if (isNaN(Number(log.split(' ')[1]))) {
+      letterLog.push(log);
+    } else {
+      digitLog.push(log);
+    }
+  });
+
+  // Sort the letter logs
+  letterLog.sort((a, b) => {
+    const logA = a.split(' ').slice(1).join(' ');
+    const logB = b.split(' ').slice(1).join(' ');
+
+    // if equal, sort by tag
+    if (logA.localeCompare(logB) === 0) {
+      console.log(a);
+      return a.localeCompare(b);
+    } else return logA.localeCompare(logB);
+  });
+
+  return [ ...letterLog, ...digitLog ];
+};
+
+const logs = [
+  'a1 9 2 3 1',
+  'g1 act car',
+  'zo4 4 7',
+  'ab1 off key dog',
+  'a8 act zoo'
+];
+
+const log = 'a1 9 2 3 1';
+
+console.log(reorderLogFiles(logs));
