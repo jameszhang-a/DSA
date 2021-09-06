f636427374f7e3e9b7f15c1a3aa12aa822f2eb87
James Zhang <jameszhanga@gmail.com>
2021-09-06 17:12:47 -0500

    runtime: 40.55, memory: 89.57 [DP solution]

--- problems/LC/easy/python/509_fib.py
+++ problems/LC/easy/python/509_fib.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Solution:
     def fib(self, n: int) -> int:
         """Simple recursion solution"""
@@ -6,3 +9,26 @@ class Solution:
         elif n == 0:
             return 0
         return self.fib(n-1) + self.fib(n-2)
+
+    def fib_dp(self, n: int, dp: dict = {0: 0, 1: 1}) -> int:
+        """Implement dynamic programming method"""
+        if n not in dp:
+            dp[n] = self.fib_dp(n-1) + self.fib_dp(n-2)
+        else:
+            return dp[n]
+
+        return dp[n]
+
+
+sol = Solution()
+
+start = time.time()
+ans = sol.fib(35)
+end = time.time()
+print(f"recursion - ans: {ans}, time: {end-start}")
+
+
+start = time.time()
+ans = sol.fib_dp(250)
+end = time.time()
+print(f"dp - ans: {ans}, time: {end-start}")
