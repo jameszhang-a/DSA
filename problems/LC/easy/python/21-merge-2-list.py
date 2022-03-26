022d90e4a94179e133dca49a321e0889868ca0bb
James Zhang <jameszhanga@gmail.com>
2022-03-26 12:11:55 -0500

    [runtime: 88.07, memory: 83.99, time: 18.50]

--- problems/LC/easy/python/21-merge-2-list.py
+++ problems/LC/easy/python/21-merge-2-list.py
@@ -1,3 +1,23 @@
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+"""
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -6,43 +26,28 @@ class ListNode:
 
     def print_list(node):
         li = []
-        while(node is not None):
+        while node is not None:
             li.append(node.val)
             node = node.next
         print(li)
 
 
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1:
-            return l2
-        elif not l2:
-            return l1
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        result = ListNode()
+        head = result
+        while list1 and list2:
+            if list1.val >= list2.val:
+                result.next = list2
+                list2 = list2.next
 
-        fin_list = None
+            elif list1.val < list2.val:
+                result.next = list1
+                list1 = list1.next
 
-        while l1 and l2:
-            if fin_list is None:
-                if l1.val < l2.val:
-                    fin_list = ListNode(l1.val)
-                    l1 = l1.next
-                else:
-                    fin_list = ListNode(l2.val)
-                    l2 = l2.next
-                temp = fin_list
-                continue
-
-            if l1 and l2:
-                if l1.val < l2.val:
-                    temp.next = ListNode(l1.val)
-                    l1 = l1.next
-                else:
-                    temp.next = ListNode(l2.val)
-                    l2 = l2.next
-
-            temp = temp.next
-        temp.next = l1 or l2
-        return fin_list
+            result = result.next
+        result.next = list1 or list2
+        return head.next
 
 
 n1 = ListNode(1)
@@ -60,4 +65,4 @@ n2.next.next = ListNode(4)
 ListNode.print_list(n2)
 
 sol = Solution()
-ListNode.print_list(sol.mergeTwoLists(n1, n2))
+ListNode.print_list(sol.mergeTwoLists(n1, n2))  # want 1, 1, 2, 3, 4, 4, 7, 9
