ef8953ba9bb82ef936cfb1325edb86477e65747b
James Zhang <jameszhanga@gmail.com>
2022-03-26 03:36:03 -0500

    [runtime: 90.01, memory: 75.79, time: 15.22]

---
+++ problems/LC/easy/python/20_valid_paren.py
@@ -0,0 +1,49 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        key = {")": "(", "]": "[", "}": "{"}
+        for c in s:
+            if c not in key:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+
+                if key[c] == stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(c)
+
+        return not stack
+
+
+sol = Solution()
+nums = [3, 2, 4]
+target = 6
+s = "(])"
+
+print(sol.isValid(s))
