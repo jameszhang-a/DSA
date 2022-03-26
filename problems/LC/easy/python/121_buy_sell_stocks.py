c3ed95ad7637b85e77fe0a6c59804a635686bf7b
James Zhang <jameszhanga@gmail.com>
2022-03-26 16:06:43 -0500

    [runtime: 34.53, memory: 87.46, time: 7.18] slower for some reason

--- problems/LC/easy/python/121_buy_sell_stocks.py
+++ problems/LC/easy/python/121_buy_sell_stocks.py
@@ -1,3 +1,26 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock
+and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction.
+If you cannot achieve any profit, return 0.
+
+
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+"""
+
 from typing import List
 
 
@@ -6,16 +29,16 @@ class Solution:
         profit = 0
         low = float("inf")
 
-        for i in range(len(prices) - 1):
-            if prices[i] < low:
-                low = prices[i]
-
-            diff = prices[i + 1] - low
-            if diff > profit:
-                profit = diff
+        for price in prices:
+            profit = max(profit, price - low)
+            low = min(low, price)
 
         return profit
 
 
-s = Solution()
-print(s.maxProfit([13, 5, 6, 1, 9]))
+sol = Solution()
+nums = [7, 6, 4, 3, 1]
+target = 6
+s = "test"
+
+print(sol.maxProfit(nums))
