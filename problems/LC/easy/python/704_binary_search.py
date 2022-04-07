08db20f4276c32c8e2c49644acd7614f727c5aaa
James Zhang <jameszhanga@gmail.com>
2022-04-07 16:57:28 -0500

    [runtime: 79.31, memory: 24.93, time: 10]

---
+++ problems/LC/easy/python/704_binary_search.py
@@ -0,0 +1,65 @@
+"""
+Given an array of integers nums which is sorted in ascending order,
+and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+"""
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            # this prevents int overflow error if left and right are large enough
+            mid = (l + r) >> 1
+            if target > nums[mid]:
+                l = mid + 1
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                return mid
+
+        return -1
+
+    def test(self, inputs, outputs):
+        for i, input in enumerate(inputs):
+            if self.search(input[0], input[1]) != outputs[i]:
+                print(f"{i} failed")
+                continue
+
+            print(f"{i} pass")
+
+
+input = [
+    [[-1, 0, 3, 5, 9, 12], 13],
+    [[-1, 0, 3, 5, 9, 12], 9],
+    [[-1, 0, 3, 5, 9, 12], 2],
+]
+expected = [
+    -1,
+    4,
+    -1,
+]
+
+
+sol = Solution()
+nums = [3, 2, 4]
+target = 6
+s = "A man, a plan, a canal: Panama"
+
+sol.test(input, expected)
