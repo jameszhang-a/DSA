7b00d573826106e50accc052cb0144712affc1a0
James Zhang <jameszhanga@gmail.com>
2022-04-08 00:50:11 -0500

    [runtime: 60.30, memory: 41.35, time: 28.07]

--- problems/LC/easy/python/53_max_subarray.py
+++ problems/LC/easy/python/53_max_subarray.py
@@ -3,22 +3,21 @@ Given an integer array nums, find the contiguous subarray (containing at least o
 
 A subarray is a contiguous part of an array.
 
- 
+
 
 Example 1:
-
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
-Example 2:
 
+Example 2:
 Input: nums = [1]
 Output: 1
-Example 3:
 
+Example 3:
 Input: nums = [5,4,-1,7,8]
 Output: 23
- 
+
 
 Constraints:
 
@@ -27,20 +26,40 @@ Constraints:
 """
 
 
+from typing import List
+
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        globalMax = localMax = nums[0]
 
-        cur, rt, i = nums[0], nums[0], 0
+        for num in nums[1:]:
+            localMax = max(num, localMax + num)
+            globalMax = max(globalMax, localMax)
 
-        while i < len(nums) - 1:
-            i += 1
-            if cur + nums[i] < nums[i]:
-                cur = nums[i]
-            else:
-                cur += nums[i]
+        return globalMax
 
-            rt = max(cur, rt)
+    def test(self, inputs, outputs):
+        for i, input in enumerate(inputs):
+            if (ans := self.maxSubArray(*input)) != outputs[i]:
+                print(f"{i} failed. Expected: {outputs[i]}, got: {ans}")
+                continue
 
-        return rt
+            print(f"{i} pass")
+
+
+input = [
+    [[-2, 1, -3, 4, -1, 2, 1, -5, 4]],
+    [[1]],
+    [[5, 4, -1, 7, 8]],
+]
+expected = [
+    6,
+    1,
+    23,
+]
+
+
+sol = Solution()
+
+sol.test(input, expected)
