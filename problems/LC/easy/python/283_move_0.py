c10797e0aca17ad575a498892ee4a2691082c510
James Zhang <jameszhanga@gmail.com>
2022-05-01 23:23:25 -0500

    [runtime: 78.43, memory: 18.64, time: 13.45]

---
+++ problems/LC/easy/python/283_move_0.py
@@ -0,0 +1,50 @@
+"""
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+"""
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                print(i, j, nums)
+                i += 1
+
+    def test(self, inputs, outputs):
+        for i, input in enumerate(inputs):
+            if (out := self.moveZeroes(*input)) != outputs[i]:
+                print(f"{i} failed")
+                print(f"Output:	 {out}")
+                print(f"Correct: {outputs[i]}")
+                print("")
+                continue
+
+            print(f"{i} pass")
+
+
+input = [
+    [[0, 1, 0, 3, 12]],
+]
+expected = [
+    True,
+    False,
+]
+
+
+sol = Solution()
+sol.test(input, expected)
