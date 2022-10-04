7cbee69fed1bdaa7599d6b4f1505ee48e0e3b774
James Zhang <jameszhanga@gmail.com>
2022-10-04 11:41:00 -0500

    [runtime: 54.6, memory: 46.18, time: 25]

---
+++ problems/LC/medium/python/230_kth_smallest_in_bst.py
@@ -0,0 +1,57 @@
+"""
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed)
+of all the values of the nodes in the tree.
+
+
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+
+Constraints:
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you
+need to find the kth smallest frequently, how would you optimize?
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from collections import deque
+from typing import Optional
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        i = 1
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            # now root is none and stack has first left node
+            curr = stack.pop()
+            if i == k:
+                return curr.val
+            i += 1
+            root = curr.right
+
+
+t1 = TreeNode(2, TreeNode(1), TreeNode(4, TreeNode(3), TreeNode(6)))
+t2 = TreeNode(2, TreeNode(1), TreeNode(3))
+
+sol = Solution()
+print(sol.kthSmallest(t1, 0))
+print(sol.kthSmallest(t2, 0))
