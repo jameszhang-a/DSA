70d0eb31a30e19921604a7dade1daec6534f7f21
James Zhang <jameszhanga@gmail.com>
2024-01-15 00:58:05 +0000

    implement filter

--- language-tricks/ts/array.ts
+++ language-tricks/ts/array.ts
@@ -11,11 +11,24 @@ class NewArray<T> {
         return this.elements[0];
     }
 
-    map<T>(callback: (value: any, index: number, array: any[]) => any): T[] {
+    map(callback: (value: T, index: number, array: T[]) => T): T[] {
         const res: T[] = [];
         for (let i = 0; i < this.length; i++) {
             res.push(callback(this.elements[i], i, this.elements));
         }
+        return res;
+    }
+
+    filter(callback: (val: T, idx: number, array: T[]) => boolean): T[] {
+        const res: T[] = [];
+
+        for (let i = 0; i < this.length; i++) {
+            const ele = this.elements[i];
+            if (callback(ele, i, this.elements)) {
+                res.push(ele);
+            }
+        }
+
         return res;
     }
 
@@ -24,21 +37,31 @@ class NewArray<T> {
         this.length++;
         return element;
     }
+
+    toString() {
+        return `${this.elements}`;
+    }
 }
 
-const arr = new NewArray([0, "a", 1, "b", , "x", "y", "", "z"]);
+const newArr = new NewArray([0, "a", 1, "b", , "x", "y", "", "z"]);
 
-const f = (val: number, idx: number) => {
-    return val + idx;
-};
+console.log("new arr:", newArr.toString());
+console.log(newArr.map((val, i) => val ?? "" + i));
+console.log(
+    "filter:",
+    newArr.filter((x) => {
+        return typeof x === "number"
+            ? x % 2 === 0
+            : (x ?? "").charCodeAt(0) % 2 === 0;
+    })
+);
 
-console.log(arr.map(f));
-console.log(arr.length);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, "a", "b", "c"];
 
-console.log(arr);
-arr.push("new element");
-console.log(arr.elements);
-
-const a = [1, 2, 3];
-
-a.map(f);
+console.log("arr:", arr.toString());
+console.log(
+    "filter:",
+    arr.filter((x) => {
+        return typeof x === "number" ? x % 2 === 0 : x.charCodeAt(0) % 2 === 0;
+    })
+);
